f63fd1b07fc85862e196299e0f6b40f029a5dfa5
Christ Akkermans <c.akkermans@vereyon.nl>
2020-05-16 17:01:21 +0200

    Remove obsolete short hand methods. Move short hand methods to extension class to slim down interface. Slim down IFlashMessage interface.

--- Web.FlashMessage.Core/FlashMessageExtensions.cs
+++ Web.FlashMessage.Core/FlashMessageExtensions.cs
@@ -22,5 +22,59 @@ namespace Vereyon.Web
             services.AddScoped<IFlashMessageSerializer, JsonFlashMessageSerializer>();
             services.AddScoped<IFlashMessage, FlashMessage>();
         }
+
+        /// <summary>
+        /// Queues a flash message for display of the specified type and with the specified message and title.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="title"></param>
+        /// <param name="messageType"></param>
+        /// <param name="isHtml"></param>
+        public static void Queue(this IFlashMessage flashMessage, FlashMessageType messageType, string message, string title = null, bool isHtml = false)
+        {
+
+            // Append the new message.
+            var messageModel = new FlashMessageModel { IsHtml = isHtml, Message = message, Title = title, Type = messageType };
+            flashMessage.Queue(messageModel);
+        }
+
+        /// <summary>
+        /// Formats and queues the passed message as an informational message with title.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        public static void Info(this IFlashMessage flashMessage, string message, string title = null)
+        {
+            flashMessage.Queue(FlashMessageType.Info, message, title);
+        }
+
+        /// <summary>
+        /// Queues the passed message as a warning message with optional title.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        public static void Warning(this IFlashMessage flashMessage, string message, string title = null)
+        {
+            flashMessage.Queue(FlashMessageType.Warning, message, title);
+        }
+
+        /// <summary>
+        /// Queues the passed message as a danger message with title.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Danger(this IFlashMessage flashMessage, string message, string title = null)
+        {
+            flashMessage.Queue(FlashMessageType.Danger, message, title);
+        }
+
+        /// <summary>
+        /// Queues the passed message as a confirmation message with title.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        public static void Confirmation(this IFlashMessage flashMessage, string message, string title = null)
+        {
+            flashMessage.Queue(FlashMessageType.Confirmation, message, title);
+        }
     }
 }
